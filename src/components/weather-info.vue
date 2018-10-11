7f138d6d6e99252ba6de27b06c6f91c29dbe1ecb
PeiXueYang <pei1825540@126.com>
2018-10-11 17:54:59 +0800

    完成了首页部分

---
+++ src/components/weather-info.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="box">
+      <div class="guide">
+          <div class="item">
+             <div class="date">今天</div>
+             <div class="temperature i">21 ~ 15℃</div>
+             <div class="weather i">🌤多云</div>
+             <div class="wind i">北风3-4级</div>
+          </div>
+          <div class="item">
+             <div class="date">周五</div>
+             <div class="temperature i">21 ~ 15℃</div>
+             <div class="weather i">🌤小雨</div>
+             <div class="wind i">北风3-4级</div>
+          </div>
+          <div class="item">
+             <div class="date">周六</div>
+             <div class="temperature i">21 ~ 15℃</div>
+             <div class="weather i">⛅晴</div>
+             <div class="wind i">北风3-4级</div>
+           </div>
+           <div class="item">
+             <div class="date">周日</div>
+             <div class="temperature i">21 ~ 15℃</div>
+             <div class="weather i">🌤多云</div>
+             <div class="wind i">北风3-4级</div>
+          </div>
+      </div>
+      <div class="livingIndex">
+          <div class="item-zhishu">
+              <img src="/static/img/clothing.png" alt="" class="icon">
+              <div class="right">
+                  <div class="key">穿衣指数 较舒适</div>
+                  <div class="value">建议着薄外套、开衫牛仔衫裤等服装。年老体弱者应适当添加衣物，宜着夹克衫、薄毛衣等。</div>
+              </div>
+          </div>
+           <div class="item-zhishu">
+              <img src="/static/img/carwashing.png" alt="" class="icon">
+              <div class="right">
+                  <div class="key">洗车指数 较适宜</div>
+                  <div class="value">较适宜洗车，未来一天无雨，风力较小，擦洗一新的汽车至少能保持一天。</div>
+              </div>
+          </div>
+           <div class="item-zhishu">
+              <img src="/static/img/pill.png" alt="" class="icon">
+              <div class="right">
+                  <div class="key">感冒指数 较易发</div>
+                  <div class="value">天气较凉，较易发生感冒，请适当增加衣服。体质较弱的朋友尤其应该注意防护。</div>
+              </div>
+          </div>
+           <div class="item-zhishu">
+              <img src="/static/img/running.png" alt="" class="icon">
+              <div class="right">
+                  <div class="key">运动指数 较适宜</div>
+                  <div class="value">天气较好，但因风力稍强，户外可选择对风力要求不高的运动，推荐您进行室内运动。</div>
+              </div>
+          </div>
+           <div class="item-zhishu">
+              <img src="/static/img/sun.png" alt="" class="icon">
+              <div class="right">
+                  <div class="key">紫外线强度指数 弱</div>
+                  <div class="value">紫外线强度较弱，建议出门前涂擦SPF在12-15之间、PA+的防晒护肤品。</div>
+              </div>
+          </div>
+      </div>
+     
+    </div>
+</template>
+<script>
+export default {
+  props: ["text"]
+};
+</script>
+<style scoped>
+.guide {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  font-size: 24rpx;
+  padding-top: 20rpx;
+  background: rgba(0, 0, 0, 0.6);
+  margin-bottom: 20rpx;
+}
+.item {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+.i {
+  padding-bottom: 20rpx;
+}
+.key{
+    font-size: 30rpx;
+}
+.livingIndex {
+  background: rgba(0, 0, 0, 0.6);
+  margin-bottom: 20rpx;
+}
+.item-zhishu {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  border-bottom: 1rpx solid rgba(250, 250, 250, 0.2);
+  padding: 20rpx 0;
+  margin: 0 30rpx;
+}
+.icon {
+  width: 80rpx;
+  height: 80rpx;
+  padding: 0 24rpx;
+}
+.right {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  flex: 1;
+}
+.value {
+  margin-top: 10rpx;
+  font-size: 24rpx;
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,105 +1,228 @@
 <template>
-  <div class="container" @click="clickHandle('test click', $event)">
-
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
+  <div class="container">
+    <img src="/static/img/accomplishment-adventure-clear-sky-585825.jpg" alt="" class="bg-img">
+    <div class="search">
+      <div class="warpper">
+        <img src="/static/img/search.png" alt="" class="search-img">
+        <input type="text" class="search-input" placeholder="查询其他城市" selection-start='-1' selection-end='-1' placeholder-style='color:#fff'>
       </div>
     </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
+    <div class="content">
+      <div class="avatarInfo">
+        <img src="/static/img/down-arrow.png" alt="" class="downArrow">
+      </div>
+      <div class="info">
+        <div class="city">
+          <div class="name">上海市</div>
+          <span class="time">2018-10-11 14：30 更新</span>
+        </div>
+        <div class="message">
+          天凉了，注意保暖~
+        </div>
+        <div class="temp">
+          20
+          <div class="fh">。</div>
+        </div>
+        <div class="weather">多云</div>
+        <div class="pm"> 优 38</div>
+      </div>
+      <div class="guide">
+        <weather></weather>
       </div>
     </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
+    <h1>dsddd</h1>
   </div>
 </template>
 
 <script>
-import card from '@/components/card'
-
+import weather from '@/components/weather-info'
 export default {
-  data () {
-    return {
-      motto: 'Hello World',
-      userInfo: {}
-    }
+  data() {
+    return {};
   },
-
   components: {
-    card
+    weather
   },
 
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      wx.navigateTo({ url })
-    },
-    getUserInfo () {
-      // 调用登录接口
-      wx.login({
-        success: () => {
-          wx.getUserInfo({
-            success: (res) => {
-              this.userInfo = res.userInfo
-            }
-          })
-        }
-      })
-    },
-    clickHandle (msg, ev) {
-      console.log('clickHandle:', msg, ev)
+    clickHandle(msg, ev) {
+      console.log("clickHandle:", msg, ev);
     }
   },
 
-  created () {
+  created() {
     // 调用应用实例的方法获取全局数据
-    this.getUserInfo()
   }
-}
+};
 </script>
 
 <style scoped>
-.userinfo {
+.container {
+  min-height: 100vh;
+  padding-top: 100rpx;
+  color: #fff;
+  width: 100%;
+  box-sizing: 100%;
+}
+
+.bg-img {
+  position: fixed;
+  z-index: 2;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+
+.search {
+  position: fixed;
+  z-index: 4;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 90rpx;
+  background-color: rgba(255, 255, 255, 0);
+}
+
+.warpper {
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  font-size: 28rpx;
+  height: 60rpx;
+  margin: 15rpx 50rpx;
+  padding: 0 15rpx;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1rpx solid #fff;
+}
+
+.search-img {
+  width: 14px;
+  height: 14px;
+  display: inline-block;
+  overflow: hidden;
+}
+
+.search-input {
+  display: block;
+  height: 1.4rem;
+  text-overflow: clip;
+  overflow: hidden;
+  white-space: nowrap;
+  font-family: UICTFontTextStyleBody;
+  min-height: 1.4rem;
+  color: #fff;
+  margin-left: 15rpx;
+}
+
+.content {
+  position: relative;
+  z-index: 2;
+  transition: 0.3s ease;
+  width: 100%;
+}
+
+.avatarInfo {
+  display: flex;
+  align-items: center;
+  padding: 0 50rpx;
+  margin-top: 30px;
+}
+
+.downArrow {
+  width: 30rpx;
+  height: 30rpx;
+  display: inline-block;
+  overflow: hidden;
+  margin-left: 50px;
+}
+
+.info {
+  width: 100%;
+  background: transparent;
+  margin-bottom: 30rpx;
+}
+
+.city {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+  padding: 20rpx 50rpx;
+  font-size: 66rpx;
+  color: #fff;
+}
+
+.name {
+  width: 5em;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+  overflow: hidden;
+}
+
+.time {
+  display: block;
+  text-align: right;
+  font-size: 24rpx;
+}
+
+.message {
+  font-size: 22rpx;
+  color: #fff;
+  padding: 30rpx 50rpx 0;
+  box-sizing: border-box;
+}
+
+.temp {
+  height: 360rpx;
+  font-size: 200rpx;
+  position: relative;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  font-weight: 300;
+  box-sizing: border-box;
+  overflow: hidden;
+}
+
+.weather {
+  margin-bottom: 30rpx;
+}
+
+.weather {
+  position: relative;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
   align-items: center;
 }
 
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
+.pm {
+  font-size: 24rpx;
+  height: 1em;
+  line-height: 1em;
+  padding: 10rpx 20rpx;
+  border-radius: 20rpx;
+  background: rgba(240, 240, 240, 0.2);
+  width: 100rpx;
+  margin: 0 auto;
+  text-align: center;
 }
 
-.userinfo-nickname {
-  color: #aaa;
-}
-
-.usermotto {
-  margin-top: 150px;
-}
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
-}
-
-.counter {
-  display: inline-block;
-  margin: 10px auto;
-  padding: 5px 10px;
-  color: blue;
-  border: 1px solid blue;
+.fh {
+  font-size: 55px;
+  text-align: top;
+  position: absolute;
+  top: 10px;
+  left: 65vw;
 }
 </style>
